10fe34c07b325e616a3bb80c070f24810ad84dd9
amir faishal <amirfaishal484@gmail.com>
2024-03-08 20:47:13 +0530

    changes

--- longest_subarray_of__sum.java
+++ longest_subarray_of__sum.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 import java.util.*;
 class longest_subarray_of_sum{
     public static void main(String[]args){
-        int len=0,i,j,sum,k;
-        int a[]={1,2,3,4,5,6};
+        int len=0,i,j,sum,k;                 //this program can find answer for both negative and positive 
+        int a[]={-1,1,1};
         Scanner obj=new Scanner(System.in);
         System.out.println("Enter the number whose subarray is to be found");
         k=obj.nextInt();
@@ -11,10 +11,10 @@ class longest_subarray_of_sum{
             sum=0;
             for(j=i;j<a.length;j++){
                 sum+=a[j];
-                if(sum>k){
+                if(Math.abs(sum)>k){
                     break;
                 }
-                if(sum==k){
+                if(Math.abs(sum)==k){
                     len=Math.max(len,j-i+1);
                 }
             }
