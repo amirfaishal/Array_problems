a2dcab830c5337b1a69349968abca9d5d3b195de
amir faishal <amirfaishal484@gmail.com>
2024-03-16 21:28:23 +0530

    changes

---
+++ pascals_triangle.java
@@ -0,0 +1,17 @@
+public class pascals_triangle {
+    public static void main(String[] args) {
+        int i,j,value=0;
+        for(i=1;i<=6;i++){
+            for(j=1;j<=i;j++){
+                if(i==1 || j==1){
+                     value=1;
+                }
+                else{
+                    value=value*(i-j+1)/(j-1);
+                }
+                System.out.print(value+" ");
+            }
+            System.out.println();
+        }
+    }
+}
